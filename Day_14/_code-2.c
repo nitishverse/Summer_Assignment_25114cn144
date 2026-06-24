7ac257f103960c0231fe8d074610f47acc5ac8c7
Nitish Yadav <yadavandranitish@gmail.com>
2026-06-24 23:35:16 +0530

    Day 14

---
+++ Day_14/_code-2.c
@@ -0,0 +1,18 @@
+// Write a program to Frequency of an element.
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {0, 1, 1, 1, 4}, b, freq = 0;
+    printf("Enter the number :\n");
+    scanf("%d", &b);
+    for (int i = 0; i < 5; i++)
+    {
+        if (b == a[i])
+        {
+            freq++;
+        }
+    }
+    printf("The Frequency of number is %d\n", freq);
+    return 0;
+}
---
+++ Day_14/_code-4.c
@@ -0,0 +1,21 @@
+// Write a program to Find duplicates in array
+#include <stdio.h>
+
+int main()
+{
+    int a[5] = {5, 8, 8, 4, 4};
+    int dup = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i + 1; j < 5; j++)
+        {
+            if (a[i] == a[j])
+            {
+                dup = a[i];
+                printf("The Duplicate element in Array is %d\n", dup);
+            }
+        }
+    }
+
+    return 0;
+}
