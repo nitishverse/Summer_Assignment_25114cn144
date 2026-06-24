d76e3bb1133b9abed1dbf8c6623abe36aa227c86
Nitish Yadav <yadavandranitish@gmail.com>
2026-06-24 23:27:05 +0530

    Day 9

---
+++ Day_09/_code-1.c
@@ -0,0 +1,23 @@
+/*
+Write a program to Print reverse star pattern.
+*****
+****
+***
+**
+*
+*/
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int j = 1; j<=i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day_09/_code-2.c
@@ -0,0 +1,24 @@
+/*
+Write a program to Print reverse number
+triangle.
+12345
+1234
+123
+12
+1
+*/
+#include<stdio.h>
+
+int main(){
+    for (int i = 5; i >=1; i--)
+    {
+        for (int j = 1;  j<=i; j++)
+        {
+            printf("%d",j);
+        }
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
